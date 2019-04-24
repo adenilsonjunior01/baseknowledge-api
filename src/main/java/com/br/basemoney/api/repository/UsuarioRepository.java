dafe3cc56a196b5d17671f7ff9b031d1a486f13c
adenilsonjunior01 <adenilsonn.souza@gmail.com>
2019-04-24 20:55:22 -0300

    add @Repository no usuarioRepository

--- src/main/java/com/br/basemoney/api/repository/UsuarioRepository.java
+++ src/main/java/com/br/basemoney/api/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.br.basemoney.api.repository;
 
 import com.br.basemoney.api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
